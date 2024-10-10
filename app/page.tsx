f682e55df920dabfb9e07c7527162b4c7b0b5075
Pratik Tandel <pratik.tandel@1rivet.com>
2024-10-10 19:44:25 +0530

    feat: add form with Input & Datepicker

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,41 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+"use client";
+import { Button } from "@nextui-org/button";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import { DatePicker } from "@nextui-org/date-picker";
+import { Divider } from "@nextui-org/divider";
+import { Input } from "@nextui-org/input";
+import { Camera } from "lucide-react";
+import { useState } from "react";
+import { parseDate } from "@internationalized/date";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { subtitle, title } from "@/components/primitives";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    dob: parseDate("2024-12-26"),
+  });
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleDateChange = (value: any) => {
+    console.log(`${value.year}-${value.month}-${value.day}`);
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      dob: parseDate(`${value.year}-${value.month}-${value.day}`),
+    }));
+  };
+
+  const handleSubmit = () => {
+    console.log(formData);
+    alert(JSON.stringify(formData));
+  };
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
@@ -22,7 +50,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex gap-3">
+      {/* <div className="flex gap-3">
         <Link
           isExternal
           className={buttonStyles({
@@ -42,15 +70,71 @@ export default function Home() {
           <GithubIcon size={20} />
           GitHub
         </Link>
-      </div>
+      </div> */}
 
-      <div className="mt-8">
+      {/* <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
             Get started by editing <Code color="primary">app/page.tsx</Code>
           </span>
         </Snippet>
+      </div> */}
+
+      {/* <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input label="Name" placeholder="Enter your name" type="text" />
+        <Input label="Email" placeholder="Enter your email" type="email" />
+      </div> */}
+
+      <div>
+        <Camera color="red" size={48} />
       </div>
+
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-md">NextUI</p>
+            <p className="text-small text-default-500">nextui.org</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col w-full gap-4">
+              <Input
+                label="Name"
+                name="name"
+                placeholder="Enter your name"
+                type="text"
+                value={formData.name}
+                onChange={handleChange}
+              />
+              <Input
+                label="Email"
+                name="email"
+                placeholder="Enter your email"
+                type="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <DatePicker
+                className="max-w-[284px]"
+                label="Birth date"
+                name="dob"
+                value={formData.dob}
+                onChange={handleDateChange}
+              />
+            </div>
+          </form>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Button color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <p>{JSON.stringify(formData)}</p>
     </section>
   );
 }
